2b78d14f06f31226bb1ebf46cd5027010dfe9ea3
Rubina Weinzettl <rubinaw@Rubinas-iMac.fed.home>
2025-02-13 21:14:49 +0100

    JS im Browser - Tag 3 - DOM Events - Uebung

--- RubinaWeinzettl/04_JS_im_Browser/Tag 3 - Form Events erkunden/validate_form.js
+++ RubinaWeinzettl/04_JS_im_Browser/Tag 3 - Form Events erkunden/validate_form.js
@@ -1,6 +1,42 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
- */
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registerForm");
+    const submitter = document.querySelector("button[value=register]");
+    const formData = new FormData(form,submitter); 
+    const inputUserName = document.getElementById("username");
+    const inputEmail = document.getElementById("email");
+    const inputPwd = document.getElementById("pwd");
+    const inputPwdConfirm = document.getElementById("confirmpwd");
+    const error = document.getElementById("error");
+    const success = document.getElementById("success");
+    //const output = document.getElementById("output");
 
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const username = inputUserName.value;
+        const eMail = inputEmail.value;
+        const pwd = inputPwd.value;
+        const pwdConfirm = inputPwdConfirm.value;
 
+        let isValid = true;
+        
+        var pwdLength = pwd.length;
+        if (pwdLength < 8) {
+            document.getElementById("error").innerHTML = "The password you entered is too short. Please enter a password with a minimum length of 8 characters!";
+            error.classList.remove("hidden");
+            isValid = false;
+        }
+        if (pwd !== pwdConfirm) {
+            document.getElementById("error").innerHTML = "The passwords you entered do not match!";
+            error.classList.remove("hidden");
+            isValid = false;
+        }
+        if (isValid == true) {
+            inputUserName.value = ""; 
+            inputEmail.value = ""; 
+            inputPwd.value = ""; 
+            inputPwdConfirm.value = ""; 
+            error.classList.add("hidden");
+            success.classList.remove("hidden");
+        }  
+    });
+});    
