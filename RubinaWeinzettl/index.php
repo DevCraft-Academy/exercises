92197c8f80f8ee1bac0c815cde051063678f1be7
Rubina Weinzettl <rubinaw@Rubinas-iMac.fed.home>
2025-02-21 17:42:53 +0100

    Vorbereitung Übung 2 - Responsive Design - noch nicht fertig

--- RubinaWeinzettl/index.php
+++ RubinaWeinzettl/index.php
@@ -1,6 +1,25 @@
-<?php
-
-echo 
-     "<a href='05_Tools_Frontend_Dev/Tag 3 - Barrierefreiheit und Responsive Design/index.html'>Übung Barrierefreiheit und Responsive Design</a>";
-?>
+<!DOCTYPE html>
+<!--lang attribute added -->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Übersicht Übungen DevCraft Academy</title>
+    </head>
+    <body>
+        <!-- used more semantic elemets, f.ex. header for heading, section und article text and image based section, section and form for the form and an extra session for the alert button -->
+        <header>
+            <!--Die Klasse header brauchen wir hier nicht mehr, weil der Style über h1 festgelegt ist -->
+            <h1>Übersicht Übungen DevCraft Academy (ab Woche 5 "Tools für Frontend Development")</h1>
+        </header>
+        <main>
+            <h2>Übungen Woche 5 "Tools für Frontend Development"</h2>
+            <ul>
+            <li><a href='05_Tools_Frontend_Dev/Tag 3 - Barrierefreiheit und Responsive Design/UE1 - Barrierefreiheit/index.html'>Übung Barrierefreiheit und Responsive Design</a></li>
+            <li><a href='05_Tools_Frontend_Dev/Tag 3 - Barrierefreiheit und Responsive Design/UE2 - Verbesserungen Responsive Design/index.html'>Übung Barrierefreiheit und Responsive Design</a></li>
+            </ul>
+        </main>
+    </body>
+</html>    
 
