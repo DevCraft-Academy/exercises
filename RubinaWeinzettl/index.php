a269e509bf3236bde151a3a1f0b83ee143853c3b
Rubina Weinzettl <rubinaw@Rubinas-iMac.fed.home>
2025-03-02 18:40:11 +0100

    Style und herrsche - Tag 3 - CSS für Profis - Übung Erstelle eine gestylte Webseite fertig

--- RubinaWeinzettl/index.php
+++ RubinaWeinzettl/index.php
@@ -14,11 +14,16 @@
         <main>
             <h2>Übungen Woche 5 "Tools für Frontend Development"</h2>
             <ul>
-            <li><a href='/05_Tools_Frontend_Dev/Tag_3_Barrierefreiheit_Responsive_Design/UE1/index.html'>Übung Barrierefreiheit</a></li>
-            <li><a href='/05_Tools_Frontend_Dev/Tag_3_Barrierefreiheit_Responsive_Design/UE2/index.html'>Übung Responsive Design</a></li>
-            <li><a href='/05_Tools_Frontend_Dev/Tag_4_Das_Netzwerk_inspizieren/index.html'>Übung: Das Netzwerk inspizieren</a></li>
-            <li><a href='/05_Tools_Frontend_Dev/Tag_5_Performance_verbessern/index.html'>Übung: Performance verbessern</a></li>
+                <li><a href='/05_Tools_Frontend_Dev/Tag_3_Barrierefreiheit_Responsive_Design/UE1/index.html'>Übung Barrierefreiheit</a></li>
+                <li><a href='/05_Tools_Frontend_Dev/Tag_3_Barrierefreiheit_Responsive_Design/UE2/index.html'>Übung Responsive Design</a></li>
+                <li><a href='/05_Tools_Frontend_Dev/Tag_4_Das_Netzwerk_inspizieren/index.html'>Übung: Das Netzwerk inspizieren</a></li>
+                <li><a href='/05_Tools_Frontend_Dev/Tag_5_Performance_verbessern/index.html'>Übung: Performance verbessern</a></li>
             </ul>
+            <h2>Übungen Woche 6 "Style und herrsche"</h2>
+            <ul>
+                <li><a href='/06_Style_herrsche/Tag_3_CSS_Profis/index.html'>Übung: Erstelle eine gestylte Webseite</a></li>
+            </ul>
+
         </main>
     </body>
 </html>    
