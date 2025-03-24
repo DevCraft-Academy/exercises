8f535f14e368a57fcd35d0cb78fc67a1ad7cb551
Rubina Weinzettl <rubinaw@Rubinas-iMac.fed.home>
2025-03-24 15:26:28 +0100

    Security und Authentifizierung - Tag 1 - Web Security Basics - Übung fertig

---
+++ RubinaWeinzettl/08_Security_Authentifizierung/Tag_1_Web_Security_Basics/unsecure-code.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Pool } = require("pg");
+const app = express();
+// Port niemals hardcodiert in Code, sondern in Evironment Variablen / externes File
+const PORT = 3000;
+
+// DB Zugangsdaten niemals hardcodiert in Code, sondern in Evironment Variablen / externes File
+const pool = new Pool({
+  user: "dbuser",
+  host: "mydb.com",
+  database: "mydb",
+  password: "dbpassword",
+  port: 5432,
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  // keine Prepared Statements verwendet -> SQL Injection möglich (in diesem Fall besonders einfach dank hardcodierten Zugangsdaten)
+  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+  const { rows } = await pool.query(query);
+
+  // hier kann ein Angreifer dank XSS gut auslesen, ob er die richtigen Benutzerdaten hat
+  if (rows.length > 0) {
+    res.send("Erfolgreich eingeloggt!");
+  } else {
+    res.send("Login fehlgeschlagen!");
+  }
+});
+
+app.get("/userdata", async (req, res) => {
+  // Diese Zeilen werden jeder Angreifer glücklich machen, da er alle Benutzerdaten aus der Tabelle users auslesen kann + prepared Statements fehlen
+  const query = "SELECT * FROM users";
+  const { rows } = await pool.query(query);
+  res.json(rows);
+});
+
+app.put('/username', async (req, res) => {
+  const { oldUsername, newUsername } = req.body;
+    // wenn er jetzt noch Lust hat die Benutzerdaten zu verändern, wird er hier fündig werden
+    const query = `UPDATE users SET username = '${newUsername}' WHERE username = '${oldUsername}'`;
+    await pool.query(query);
+    res.send('Benutzername erfolgreich geändert.');
+});
+
+app.listen(PORT, () => {
+  // dank dieser Fehlermeldung und den hardcodierten Variablen oben ist jetzt auch klar, an welchen Port er seine Attacke wenden darf
+  console.log(`Server läuft auf Port ${PORT}`);
+});
+
+
