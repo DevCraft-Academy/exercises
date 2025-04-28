e158891c435f14c64392ba41c4376b4df86fd24e
Yazan Kassouaa <yazan.kassouaa@gmail.com>
2025-04-28 06:31:15 +0200

    TDD task 2

--- Yazanwolf/Probleme_loesen/tdd/ue2/calculator.test.js
+++ Yazanwolf/Probleme_loesen/tdd/ue2/calculator.test.js
@@ -4,16 +4,32 @@ test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
 
+test('add non numeric input throws error', () => {
+    expect(() => add('a', 2)).toThrow(Error);
+});
+
 test('subtract 4 from 10 equals 6', () => {
     expect(subtract(10, 4)).toBe(6);
+});
+
+test('subtract non numeric input throws error', () => {
+    expect(() => subtract(10, 'b')).toThrow(Error);
 });
 
 test('multiply 2 * 8 equals 16', () => {
     expect(multiply(2, 8)).toBe(16);
 });
 
+test('multiply non numeric input throws error', () => {
+    expect(() => multiply({ name: 'john' }, 'b')).toThrow(Error);
+});
+
 test('devide 18 by 2 equals 9', () => {
     expect(devide(18, 2)).toBe(9);
+});
+
+test('devide non numeric input throws error', () => {
+    expect(() => devide('x', 2).toThrow(Error));
 });
 
 test('divide any number by 0 to throw an error', () => {
