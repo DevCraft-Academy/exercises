1baff38524eeec6c18424eb0b0526b6e0ce0f459
PhilT <philipp66693@gmail.com>
2025-12-25 20:46:10 +0100

    fixed typos

--- PhilToth/optimiseSearchJS/optimise.js
+++ PhilToth/optimiseSearchJS/optimise.js
@@ -44,13 +44,13 @@ const data = Array.from({ length: 100 }, (_, i) => i);
 const target = 99;
 
 console.time("Linear Search");
-for (let i = 0; i < 100000; i++) {
+for (let i = 0; i < 100; i++) {
   linearSearch(data, target);
 }
 console.timeEnd("Linear Search");
 
 console.time("Binary Search");
-for (let i = 0; i < 100000; i++) {
+for (let i = 0; i < 100; i++) {
   binarySearch(data, target);
 }
 console.timeEnd("Binary Search");
@@ -59,19 +59,4 @@ console.timeEnd("Binary Search");
  * try it yourself with command "node optimise.js" :
  * Linear Search: ~ 50% slower
  * Binary Search: ~ 50% faster
-
-    * Linear Search: 7.59ms
-    * Binary Search: 4.066ms
-
-    * Linear Search: 8.04ms
-    * Binary Search: 4.221ms
-
-    * Linear Search: 7.303ms
-    * Binary Search: 4.055ms
-    * 
-    * Linear Search: 7.959ms
-    * Binary Search: 4.167ms
-
-    * Linear Search: 7.788ms
-    * Binary Search: 4.167ms
 */
