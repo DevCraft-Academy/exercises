5d86ca4ee9f9b51405601eff1e95c5f6e032574e
semjasa83 <marceldezanna@gmail.com>
2026-06-23 17:38:09 +0200

    be_exercise_04

---
+++ MarceldeZanna/be_exercise_04/app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+// Eine einfache In-Memory-Buch-Datenbank
+let books = [
+    { id: 1, title: "1984", author: "George Orwell" },
+    { id: 2, title: "Brave New World", author: "Aldous Huxley" },
+    { id: 3, title: "Brave New World", author: "Aldous Huxley" },
+    { id: 4, title: "Brave New World", author: "Aldous Huxley" },
+    { id: 5, title: "Brave New World", author: "Aldous Huxley" },
+    { id: 6, title: "Brave New World", author: "Aldous Huxley" },
+    { id: 7, title: "Brave New World", author: "Aldous Huxley" },
+    { id: 8, title: "Brave New World", author: "Aldous Huxley" },
+];
+
+let nextId = 2;
+
+app.use(bodyParser.json());
+
+// GET-Route für ein Buch
+app.get("/book/:id", (req, res) => {
+    const book = books.find((b) => b.id === parseInt(req.params.id));
+    if (!book) {
+        res.status(404).send("Buch nicht gefunden");
+    }
+    res.json(book);
+});
+
+// DELETE-Route für ein Buch
+app.delete('/book/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    const book = books.find(b => b.id === id)
+    if (!book) return res.status(404).send('Buch nicht gefunden')
+    books = books.filter(b => b.id !== id)
+    res.json(book)
+})
+
+// Post-Route zum Hinzufügen eines Buches
+app.post("/book", (req, res) => {
+    if (!req.body.title || !req.body.author) {
+        res.status(400).send("Titel oder Autor fehlt");
+    }
+    const newBook = {
+        id: generateId(),
+        title: req.body.title,
+        author: req.body.author,
+    };
+    books.push(newBook);
+    res.send(newBook);
+});
+
+// Route, um alle Bücher abzurufen
+app.get("/books", (req, res) => {
+    res.json(books);
+});
+
+app.listen(port, () => {
+    console.log(`Server läuft auf http://localhost:${port}`);
+});
+
+function generateId() {
+    if (!books.length) return nextId = 1;
+    const maxId = books.reduce((m, b) => Math.max(m, b.id), 0);
+    return nextId = maxId + 1;
+}
+
+module.exports = app;
