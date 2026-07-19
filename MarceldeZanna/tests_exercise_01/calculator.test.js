a4102ffaa9f5313116c892ff580982bc561bbaa9
semjasa83 <marceldezanna@gmail.com>
2026-07-19 17:35:22 +0200

    requested Changes

--- MarceldeZanna/tests_exercise_01/calculator.test.js
+++ MarceldeZanna/tests_exercise_01/calculator.test.js
@@ -7,11 +7,12 @@ const { add, subtract, multiply } = require('./calculator');
 // TODO: Write a test for the add function
 // Tip: Use the AAA Pattern (Arrange-Act-Assert)
 test('add should return sum of two numbers', () => {
+    // Arrange
     const a = 5;
     const b = 30;
-
+    // Act
     const result = add(a, b)
-
+    // Assert
     expect(result).toBe(35)
 })
 
@@ -19,11 +20,12 @@ test('add should return sum of two numbers', () => {
 // TODO: Write a test for the subtract function
 // Test with a larger number minus a smaller number (e.g., 10 - 4)
 test('subtract should return a smaller number', () => {
+    // Arrange
     const a = 8;
     const b = 4;
-
+    // Act
     const result = subtract(a, b);
-
+    // Assert
     expect(result).toBe(4)
 })
 
@@ -31,10 +33,11 @@ test('subtract should return a smaller number', () => {
 // TODO: Write a test for multiply with 0 as an edge case
 // What happens when you multiply a number by 0?
 test('multiply a number by 0', () => {
+    // Arrange
     const a = 5
     const b = 0
-
+    // Act
     const result = multiply(a, b)
-
+    // Assert
     expect(result).toBe(0)
 })
