caa8016dc984b0dfa45e54b66be42ac2035126da
semjasa83 <marceldezanna@gmail.com>
2026-07-19 22:36:16 +0200

    tests

--- MarceldeZanna/tests_exercise_04/emailValidator.test.js
+++ MarceldeZanna/tests_exercise_04/emailValidator.test.js
@@ -17,6 +17,73 @@ describe('validateEmail - Happy Path', () => {
   });
 });
 
+describe('validateEmail - Boundary Cases', () => {
+  test('should accept minimum length email', () => {
+    expect(validateEmail('a@b.c')).toBe(true);
+  });
+
+  test('should reject email that is too short', () => {
+    expect(() => validateEmail('a@b')).toThrow('Invalid email format');
+  });
+
+  test('should reject email that is too long', () => {
+    function repeatLetter(letter, count) {
+      return letter.repeat(count);
+    };
+    expect(() => validateEmail(`${repeatLetter('a', 255)}@b.c`)).toThrow('Invalid email is too long')
+  });
+});
+
+describe('validateEmail - Null/Undefined/EmptyCase', () => {
+  test('should not accept NULL', () => {
+    expect(() => validateEmail(null)).toThrow('Email is required')
+  });
+
+  test('should not be undefined', () => {
+    expect(() => validateEmail(undefined)).toThrow('Email is required')
+  });
+
+  test('should not be empty string', () => {
+    expect(() => validateEmail('')).toThrow('Email is required')
+  });
+});
+
+describe('validateEmail - Invalid Format Cases', () => {
+  test('should contain one @', () => {
+    expect(() => validateEmail('ab.c')).toThrow('Please enter a valid email address')
+  });
+
+  test('should not contain more than one @', () => {
+    expect(() => validateEmail('a@b@.c')).toThrow('Please enter a valid email address')
+  });
+
+  test('should not allow consecutive dots after @', () => {
+    expect(() => validateEmail('user@domain..com')).toThrow('Please enter a valid email address');
+  });
+});
+
+describe('validateEmail - Special Characters', () => {
+  test('should accept plus sign in local part', () => {
+    expect(validateEmail('user+tag@example.com')).toBe(true);
+  });
+
+  test('should accept underscore in local part', () => {
+    expect(validateEmail('user_name@example.com')).toBe(true);
+  });
+
+  test('should reject spaces inside the email', () => {
+    expect(() => validateEmail('user @example.com')).toThrow('Please enter a valid email address');
+  });
+
+  test('should reject invalid special characters', () => {
+    expect(() => validateEmail('user,example@example.com')).toThrow('Please enter a valid email address');
+  });
+
+  test('should reject leading dot in local part', () => {
+    expect(() => validateEmail('.user@example.com')).toThrow('Please enter a valid email address');
+  });
+});
+
 // TODO: Add your edge case tests here!
 //
 // Use the checklists from the article:
